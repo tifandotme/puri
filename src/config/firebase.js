da99317925d940ec9d00a71379acdce9765c23c0
Tifan <tifan@proton.me>
2023-02-11 18:57:01 +0700

    update database url

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,8 +5,6 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyD6eq4vdO_B0ivZN5oa68W97f_hSd0lMuk",
   projectId: "puri-systems",
-  databaseURL:
-    "https://puri-systems-default-rtdb.asia-southeast1.firebasedatabase.app",
   storageBucket: "puri-systems.appspot.com",
   messagingSenderId: "684555146850",
   appId: "1:684555146850:web:158162497d3aff2b73233b",
@@ -15,7 +13,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const database = getDatabase(app);
+const database = getDatabase(
+  app,
+  "https://puri-systems-default-rtdb.asia-southeast1.firebasedatabase.app"
+);
 
 // Do not use emulator in production! delete this line before deploying
 // import { connectAuthEmulator } from "firebase/auth";
