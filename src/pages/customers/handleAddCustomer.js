d84c0e84dff37586dd7360edef5b7a5bd0668d63
Tifan <tifan@proton.me>
2023-03-03 07:03:55 +0700

    implement onsubmit on AddCustomer

---
+++ src/pages/customers/handleAddCustomer.js
@@ -0,0 +1,26 @@
+import { set, ref, push, serverTimestamp } from "firebase/database";
+import { database } from "../../config/firebase";
+
+async function handleAddCustomer(data, setLoading, navigate) {
+  try {
+    setLoading(true);
+
+    await set(push(ref(database, "customers")), {
+      ...data,
+      CreatedAt: serverTimestamp(),
+    });
+
+    navigate("/customers");
+  } catch (error) {
+    // toast({
+    //   title: "Email sudah terdaftar",
+    //   status: "error",
+    //   duration: 3000,
+    // });
+    console.log(error);
+  } finally {
+    setLoading(false);
+  }
+}
+
+export default handleAddCustomer;
