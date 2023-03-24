a8dc7db8fb637243d8546d5816d4ebd103e49d44
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-03-24 17:24:27 +0700

    🔀 chore(customer.d.ts): rearrange properties in Customer type ✨ feat(customer.d.ts): add sales property to Customer type

--- src/@types/customer.d.ts
+++ src/@types/customer.d.ts
@@ -1,16 +1,17 @@
 type Customer = {
-  createdAt: number;
+  name: string;
+  id: number;
+  phone: number;
+  phone2?: number;
   address: {
     city: string;
     district: string;
     regency: string;
     street: string;
   };
-  id: number;
-  name: string;
-  phone: number;
-  phone2?: number;
   type: "individu" | "perusahaan";
+  createdAt: number;
+  sales: string;
 };
 
 type CustomerList = Record<string, Customer>;
