4ba07ce73969e1fbdf31db1a7114c903b2bd6fd4
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-03-24 17:28:28 +0700

    feat(App.tsx): add AppContext to provide customerList and isLoading to child components feat(App.tsx): add EditCustomer component to handle customer editing
    refactor(App.tsx): extract customer list context provider to separate component
    feat(App.tsx): add routes for customer list, add customer, and edit customer pages
    
    refactor(main.tsx): remove unused imports and commented code
    style(main.tsx): format code for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Route, Routes, useLocation } from "react-router-dom";
 
@@ -11,11 +11,23 @@ import Login from "./pages/auths/Login";
 import SignUp from "./pages/auths/SignUp";
 import ForgotPassword from "./pages/auths/ForgotPassword";
 
-import MainContainer from "./pages/dashboard/PanelContainer";
+import PanelContainer from "./pages/dashboard/PanelContainer";
 import AddCustomer from "./pages/customers/AddCustomer";
 import OrderList from "./pages/orders/OrderList";
 import CustomerList from "./pages/customers/CustomerList";
 import Home from "./pages/home/Home";
+import useCustomerList from "./hooks/useCustomerList";
+import EditCustomer from "./pages/customers/EditCustomer";
+
+type APType = {
+  customerList: CustomerList | undefined;
+  isLoading: boolean;
+};
+
+export const AppContext = createContext<APType>({
+  customerList: undefined,
+  isLoading: true,
+});
 
 function App() {
   const currentPath = useLocation().pathname;
@@ -24,55 +36,58 @@ function App() {
   const [isPageLoading, setPageLoading] = useState(true);
 
   useEffect(() => {
-    const authState = auth.onAuthStateChanged((user) => {
-      user ? setCurrentUser(user) : setCurrentUser(undefined);
-
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user ? user : undefined);
       setPageLoading(false);
     });
 
-    return () => authState();
+    return unsubscribe;
   }, []);
 
   return (
     <ChakraProvider theme={customTheme(currentPath, isPageLoading)}>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainContainer
-              currentUser={currentUser}
-              isPageLoading={isPageLoading}
-              currentPath={currentPath}
-            />
-          }
-        >
-          <Route index element={<Home />} />
-          <Route path="customers">
-            <Route index element={<CustomerList />} />
-            <Route path="new" element={<AddCustomer />} />
-          </Route>
-
-          <Route path="orders" element={<OrderList />} />
+      <AppContext.Provider value={useCustomerList(currentUser)}>
+        <Routes>
           <Route
-            path="help"
-            element={<h1>Bantuan TODO: dokumentasi lengkap cara penggunaan</h1>}
-          />
-        </Route>
-        <Route
-          element={
-            <AuthContainer
-              currentUser={currentUser}
-              isPageLoading={isPageLoading}
-              currentPath={currentPath}
+            path="/"
+            element={
+              <PanelContainer
+                currentUser={currentUser}
+                isPageLoading={isPageLoading}
+              />
+            }
+          >
+            <Route index element={<Home />} />
+            <Route path="customers">
+              <Route index element={<CustomerList />} />
+              <Route path="new" element={<AddCustomer />} />
+              <Route path="edit" element={<EditCustomer />} />
+            </Route>
+
+            <Route path="orders" element={<OrderList />} />
+            <Route
+              path="help"
+              element={
+                <h1>Bantuan TODO: dokumentasi lengkap cara penggunaan</h1>
+              }
             />
-          }
-        >
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-        </Route>
-        <Route path="*" element={<h1>404</h1>} />
-      </Routes>
+          </Route>
+          <Route
+            element={
+              <AuthContainer
+                currentUser={currentUser}
+                isPageLoading={isPageLoading}
+                currentPath={currentPath}
+              />
+            }
+          >
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+          </Route>
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </AppContext.Provider>
     </ChakraProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,21 @@ import ReactDOM from "react-dom/client";
 // import { StrictMode } from "react";
 
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import App from "./App";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root") as Element;
 
 ReactDOM.createRoot(rootElement).render(
   // <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    {/* <QueryClientProvider client={queryClient}> */}
+    <App />
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    {/* </QueryClientProvider> */}
+  </BrowserRouter>
   // </StrictMode>
 );
