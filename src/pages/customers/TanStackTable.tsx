2f49f4c3055c3d5f065f91fe51d94e8464e6b466
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-04 21:49:06 +0700

    refactor(TanStackTable.tsx): refactor TanStackTable component to use generic types and add SearchBar and Pagination components

--- src/pages/customers/TanStackTable.tsx
+++ src/pages/customers/TanStackTable.tsx
@@ -16,11 +16,47 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
-import { Table as TanstackTable, flexRender } from "@tanstack/react-table";
+import {
+  ColumnDef,
+  Row,
+  Table as TTable,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
-function TanStackTable<T>({ table }: { table: TanstackTable<T> }) {
+// const columns: ColumnDef<[string, Customer<string>]>[]
+// const data: [string, any][]
+
+// type TSTProps = {
+//   data: [string, any][];
+//   columns: ColumnDef<[string, Customer<string>]>[];
+// };
+
+type TSTProps<Data extends any[], Columns extends ColumnDef<any, any>[]> = {
+  data: Data;
+  columns: Columns;
+};
+
+function TanStackTable<
+  Data extends any[],
+  Columns extends ColumnDef<any, any>[]
+>({ data, columns }: TSTProps<Data, Columns>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    debugTable: false,
+  });
+
   return (
     <>
+      <SearchBar table={table} />
+
       <TableContainer>
         <Table variant="striped">
           <Thead>
@@ -29,19 +65,12 @@ function TanStackTable<T>({ table }: { table: TanstackTable<T> }) {
                 {headerGroup.headers.map((header) => (
                   <Th
                     key={header.id}
-                    style={{
-                      width:
-                        header.getSize() !== 150
-                          ? header.getSize() + "%"
-                          : undefined,
-                    }}
-                    {...(header.index === 2 && {
-                      display: { base: "none", lg: "table-cell" },
-                    })}
-                    {...(header.index === 0 && {
-                      textAlign: "center",
-                      paddingRight: 0,
-                    })}
+                    width={
+                      header.getSize() !== 150
+                        ? header.getSize() + "%"
+                        : undefined
+                    }
+                    {...header.column.columnDef.meta?.headerProps}
                   >
                     {header.isPlaceholder
                       ? null
@@ -58,7 +87,7 @@ function TanStackTable<T>({ table }: { table: TanstackTable<T> }) {
             {table.getRowModel().rows.map((row) => (
               <Tr key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <Td key={cell.id} {...cell.column.columnDef.meta}>
+                  <Td key={cell.id} {...cell.column.columnDef.meta?.bodyProps}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </Td>
                 ))}
@@ -67,78 +96,104 @@ function TanStackTable<T>({ table }: { table: TanstackTable<T> }) {
           </Tbody>
         </Table>
       </TableContainer>
-      <Stack
-        m="5"
-        direction={{ base: "column", lg: "row" }}
-        gap={{ base: 0, lg: 7 }}
-        justifyContent={{ base: "flex-start", lg: "space-between" }}
-      >
-        <HStack>
-          <ButtonGroup colorScheme="blue" size="sm">
-            <Button
-              onClick={() => table.setPageIndex(0)}
-              isDisabled={!table.getCanPreviousPage()}
-            >
-              {"<<"}
-            </Button>
-            <Button
-              onClick={() => table.previousPage()}
-              isDisabled={!table.getCanPreviousPage()}
-            >
-              {"<"}
-            </Button>
-            <Button
-              onClick={() => table.nextPage()}
-              isDisabled={!table.getCanNextPage()}
-            >
-              {">"}
-            </Button>
-            <Button
-              onClick={() => table.nextPage()}
-              isDisabled={!table.getCanNextPage()}
-            >
-              {">>"}
-            </Button>
-          </ButtonGroup>
 
-          <InputGroup w="auto" size="sm">
-            <InputLeftAddon>Ke Halaman: </InputLeftAddon>
-            <Input
-              type="number"
-              defaultValue={table.getState().pagination.pageIndex + 1}
-              onChange={(e) => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                if (page < table.getPageCount()) table.setPageIndex(page);
-              }}
-              w="3em"
-            />
-          </InputGroup>
-        </HStack>
-        <HStack>
-          <Text fontSize="sm">
-            Halaman{" "}
-            <strong>
-              {table.getState().pagination.pageIndex + 1} dari{" "}
-              {table.getPageCount()}{" "}
-            </strong>
-          </Text>
-          <Select
-            w="auto"
-            size="sm"
-            value={table.getState().pagination.pageSize}
-            onChange={(e) => {
-              table.setPageSize(Number(e.target.value));
-            }}
-          >
-            {[10, 20, 30, 40, 50].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                Lihat {pageSize}
-              </option>
-            ))}
-          </Select>
-        </HStack>
-      </Stack>
+      <Pagination table={table} />
     </>
+  );
+}
+
+function SearchBar<T>({ table }: { table: TTable<T> }) {
+  return (
+    <HStack my="4" mx="6" justifyContent="flex-start">
+      <Input
+        type="text"
+        value={(table.getColumn("name")?.getFilterValue() ?? "") as string}
+        onChange={(e) =>
+          table.getColumn("name")?.setFilterValue(e.target.value)
+        }
+        placeholder="Cari Nama Pelanggan .."
+        variant="flushed"
+        _placeholder={{ color: "black", opacity: 0.8 }}
+        w="60"
+      />
+    </HStack>
+  );
+}
+
+function Pagination<T>({ table }: { table: TTable<T> }) {
+  return (
+    <Stack
+      m="5"
+      direction={{ base: "column", lg: "row" }}
+      gap={{ base: 5, lg: 7 }}
+      justifyContent={{ lg: "space-between" }}
+      alignItems={{ base: "center" }}
+    >
+      <HStack>
+        <ButtonGroup colorScheme="blue" size="sm">
+          <Button
+            onClick={() => table.setPageIndex(0)}
+            isDisabled={!table.getCanPreviousPage()}
+          >
+            {"<<"}
+          </Button>
+          <Button
+            onClick={() => table.previousPage()}
+            isDisabled={!table.getCanPreviousPage()}
+          >
+            {"<"}
+          </Button>
+          <Button
+            onClick={() => table.nextPage()}
+            isDisabled={!table.getCanNextPage()}
+          >
+            {">"}
+          </Button>
+          <Button
+            onClick={() => table.nextPage()}
+            isDisabled={!table.getCanNextPage()}
+          >
+            {">>"}
+          </Button>
+        </ButtonGroup>
+
+        <InputGroup w="auto" size="sm">
+          <InputLeftAddon>Ke Halaman: </InputLeftAddon>
+          <Input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              if (page < table.getPageCount()) table.setPageIndex(page);
+            }}
+            w="3em"
+          />
+        </InputGroup>
+      </HStack>
+      <HStack>
+        <Text fontSize="sm">
+          Halaman{" "}
+          <strong>
+            {table.getState().pagination.pageIndex + 1} dari{" "}
+            {table.getPageCount()}{" "}
+          </strong>
+        </Text>
+        <Select
+          w="auto"
+          size="sm"
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Lihat {pageSize}
+            </option>
+          ))}
+        </Select>
+      </HStack>
+    </Stack>
   );
 }
 
