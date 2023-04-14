7175556707ade53dd9dd47846bbf12153970d36c
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-14 07:37:18 +0700

    feat(CustomerNameSelect.tsx): add CustomerNameSelect component for selecting customer names in orders page

---
+++ src/pages/orders/CustomerNameSelect.tsx
@@ -0,0 +1,89 @@
+import { Box, SelectFieldProps, useStyleConfig } from "@chakra-ui/react";
+import { useContext, useMemo } from "react";
+import { Control, Controller } from "react-hook-form";
+import ReactSelect, { ClassNamesConfig, Options } from "react-select";
+import { AppContext } from "../../App";
+
+function CustomerNameSelect({ control }: { control: Control }) {
+  const { customerList, isLoading } = useContext(AppContext);
+
+  // convert customerList to react-select's options format
+  // note: we can actually use our own format using getOptionLabel and getOptionValue
+  const options: Options<{ value: string; label: string }> = useMemo(
+    () =>
+      Object.entries(customerList ?? {}).map(([key, value]) => ({
+        value: key,
+        label: value.name,
+      })),
+    [customerList]
+  );
+
+  // get style config from chakra-ui's Select component
+  const { field, icon } = useStyleConfig("Select", {}) as Record<
+    "field" | "icon",
+    SelectFieldProps
+  >;
+
+  // assign class names to react-select's inner components
+  const customStyles: ClassNamesConfig = {
+    control: ({ isDisabled }) => {
+      let str = "select-field";
+      if (isDisabled) str += " select-field--is-disabled";
+      return str;
+    },
+    dropdownIndicator: () => "select-icon",
+    placeholder: () => "select-placeholder",
+  };
+
+  // provide css styles to react-select's inner components using chakra-ui's theme tokens
+  const selectChakra: Record<string, SelectFieldProps> = {
+    ".select-field": {
+      width: field.width,
+      minWidth: field.minWidth,
+      lineHeight: field.lineHeight,
+      bg: field.bg,
+      fontSize: field.fontSize,
+      px: 1,
+      h: field.h,
+      borderRadius: field.borderRadius,
+      border: field.border,
+      borderColor: field.borderColor,
+      _hover: field._hover,
+      _invalid: field._invalid,
+    },
+    ".select-field--is-disabled": {
+      opacity: field._disabled?.opacity as number,
+    },
+    ".select-icon": {
+      color: icon.color,
+      width: 33,
+      height: 34,
+    },
+    ".select-placeholder": {
+      color: icon.color,
+    },
+  };
+
+  return (
+    <Box sx={selectChakra}>
+      <Controller
+        name="customer" // the output format will be { value, label }
+        control={control}
+        render={({ field }) => (
+          <ReactSelect
+            required
+            options={options}
+            classNames={customStyles}
+            components={{
+              IndicatorSeparator: () => null, // removes the separator
+            }}
+            isLoading={isLoading}
+            {...field}
+          />
+        )}
+      />
+    </Box>
+  );
+}
+
+export default CustomerNameSelect;
