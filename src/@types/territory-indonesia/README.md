04bdd7a151e07a399130d29ff177a6a4fc983787
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-16 12:11:09 +0700

    chore(prettierignore): add src/@types/territory-indonesia to ignore list feat(territory-indonesia): add type definitions for all entities (Province, Regency, District, Village) refactor(territory-indonesia): remove individual type definition files for each entity

--- src/@types/territory-indonesia/README.md
+++
@@ -1,1 +0,0 @@
-Type definitions are not present in the package [territory-indonesia](https://www.npmjs.com/package/territory-indonesia), so I made my own.
--- src/@types/territory-indonesia/util.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "territory-indonesia" {
-  export function uCase(str: string): string;
-  export function arrObj<T>(arr: T[]): T | Undefined;
-  export function normalizeName<T extends { name: string }>(data: T | T[]): T;
-}
