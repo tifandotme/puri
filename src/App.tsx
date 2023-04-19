d9cc5a75eb1217068117af27fd9207dabd5ee33a
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-19 23:39:25 +0700

    refactor(App.tsx): use custom hooks for customer and order list feat(App.tsx): add help page refactor(App.tsx): use useUser hook for user authentication refactor(App.tsx): pass user and isLoading props to PanelContainer and AuthContainer docs(index.html): add license information in HTML comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,82 @@
-import { useState, useEffect, createContext } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { createContext } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-
-import { User } from "firebase/auth";
-import { auth } from "./config/firebase";
 import customTheme from "./config/theme";
-
+import useCustomerList from "./hooks/useCustomerList";
+import useOrderList from "./hooks/useOrderList";
+import useUser from "./hooks/useUser";
 import AuthContainer from "./pages/auths/AuthContainer";
+import ForgotPassword from "./pages/auths/ForgotPassword";
 import Login from "./pages/auths/Login";
 import SignUp from "./pages/auths/SignUp";
-import ForgotPassword from "./pages/auths/ForgotPassword";
-
-import PanelContainer from "./pages/dashboard/PanelContainer";
 import AddCustomer from "./pages/customers/AddCustomer";
-import OrderListPage from "./pages/orders/OrderListPage";
 import CustomerList from "./pages/customers/CustomerList";
-import Home from "./pages/home/Home";
-import useCustomerList from "./hooks/useCustomerList";
 import EditCustomer from "./pages/customers/EditCustomer";
+import PanelContainer from "./pages/dashboard/PanelContainer";
+import Home from "./pages/home/Home";
 import AddOrderPage from "./pages/orders/AddOrderPage";
+import OrderListPage from "./pages/orders/OrderListPage";
 
-type APType = {
-  customerList: CustomerList | undefined;
-  isLoading: boolean;
-};
-
-export const AppContext = createContext<APType>({
+export const CustomerListContext = createContext<CustomerListContext>({
   customerList: undefined,
+  isLoading: true,
+});
+
+export const OrderListContext = createContext<OrderListContext>({
+  orderList: undefined,
   isLoading: true,
 });
 
 function App() {
   const currentPath = useLocation().pathname;
 
-  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
-  const [isPageLoading, setPageLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user ? user : undefined);
-      setPageLoading(false);
-    });
-
-    return unsubscribe;
-  }, []);
+  const { user, isLoading } = useUser();
 
   return (
-    <ChakraProvider theme={customTheme(currentPath, isPageLoading)}>
-      <AppContext.Provider value={useCustomerList(currentUser)}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PanelContainer
-                currentUser={currentUser}
-                isPageLoading={isPageLoading}
-              />
-            }
-          >
-            <Route index element={<Home />} />
-            <Route path="customers">
-              <Route index element={<CustomerList />} />
-              <Route path="new" element={<AddCustomer />} />
-              <Route path="edit" element={<EditCustomer />} />
-            </Route>
+    <ChakraProvider theme={customTheme(currentPath, isLoading)}>
+      <CustomerListContext.Provider value={useCustomerList(user)}>
+        <OrderListContext.Provider value={useOrderList(user)}>
+          <Routes>
+            <Route
+              path="/"
+              element={<PanelContainer user={user} isLoading={isLoading} />}
+            >
+              <Route index element={<Home />} />
+              <Route path="customers">
+                <Route index element={<CustomerList />} />
+                <Route path="new" element={<AddCustomer />} />
+                <Route path="edit" element={<EditCustomer />} />
+              </Route>
 
-            <Route path="orders">
-              <Route index element={<OrderListPage />} />
-              <Route path="new" element={<AddOrderPage />} />
-              {/* <Route path="edit" element={<EditOrder />} /> */}
+              <Route path="orders">
+                <Route index element={<OrderListPage />} />
+                <Route path="new" element={<AddOrderPage />} />
+                {/* <Route path="edit" element={<EditOrder />} /> */}
+              </Route>
+              <Route
+                path="help"
+                element={
+                  <h1>Bantuan TODO: dokumentasi lengkap cara penggunaan</h1>
+                }
+              />
             </Route>
             <Route
-              path="help"
               element={
-                <h1>Bantuan TODO: dokumentasi lengkap cara penggunaan</h1>
+                <AuthContainer
+                  user={user}
+                  isLoading={isLoading}
+                  currentPath={currentPath}
+                />
               }
-            />
-          </Route>
-          <Route
-            element={
-              <AuthContainer
-                currentUser={currentUser}
-                isPageLoading={isPageLoading}
-                currentPath={currentPath}
-              />
-            }
-          >
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgotpassword" element={<ForgotPassword />} />
-          </Route>
-          <Route path="*" element={<h1>404</h1>} />
-        </Routes>
-      </AppContext.Provider>
+            >
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/forgotpassword" element={<ForgotPassword />} />
+            </Route>
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </OrderListContext.Provider>
+      </CustomerListContext.Provider>
     </ChakraProvider>
   );
 }
