404d2623e7eecba15c4510132c84233e98e5588d
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-21 17:03:08 +0700

    chore: remove Firebase hosting deployment workflows fix(firebase.ts): update authDomain to puri-systems.firebaseapp.com fix(env.d.ts): update VITE_FIREBASE_MESSAGING_SENDER_ID variable name

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,9 +8,9 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   projectId: "puri-systems",
-  authDomain: "puri-systems.web.app",
+  authDomain: "puri-systems.firebaseapp.com",
   storageBucket: "puri-systems.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   databaseURL:
     "https://puri-systems-default-rtdb.asia-southeast1.firebasedatabase.app",
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,7 +2,7 @@ interface ImportMetaEnv {
   DEV: boolean;
   VITE_FIREBASE_API_KEY: string;
   VITE_FIREBASE_APP_ID: string;
-  VITE_FIREBASE_MESSAGING_ID: string;
+  VITE_FIREBASE_MESSAGING_SENDER_ID: string;
 }
 
 interface ImportMeta {
