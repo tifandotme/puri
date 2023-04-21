0746db833909d3eda047e60302404141ac143448
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-21 14:30:58 +0700

    refactor(handle-add-customer.ts): remove commented code and unused imports feat(handle-add-customer.ts): use data from form to create customer object and push it to firebase database fix(handle-add-customer.ts): navigate to /customers after adding customer to database

--- src/pages/customers/handle-add-customer.ts
+++ src/pages/customers/handle-add-customer.ts
@@ -1,35 +1,22 @@
 import { useToast } from "@chakra-ui/react";
 import { push, ref, serverTimestamp, set } from "firebase/database";
-import { FieldValues } from "react-hook-form";
 import { NavigateFunction } from "react-router-dom";
 import { auth, database } from "../../config/firebase";
 import { capitalizeWords } from "../../utils/utils";
 
 async function handleAddCustomer(
-  data: FieldValues,
+  data: CustomerForm,
   navigate: NavigateFunction,
   toast: ReturnType<typeof useToast>
 ) {
   try {
     const { name, id, phone, phone2, address, type, prefixName } = data;
 
-    // const customer: Customer<ReturnType<typeof serverTimestamp>> = {
-    //   name: capitalizeWords(name) + (prefixName ? ", " + prefixName : ""),
-    //   id,
-    //   phone,
-    //   ...(phone2 && { phone2 }),
-    //   address,
-    //   type,
-    //   createdAt: serverTimestamp(),
-    //   sales: auth.currentUser?.uid,
-    // };
-
-    // temporary, for testing
     const customer: Customer<ReturnType<typeof serverTimestamp>> = {
       name: capitalizeWords(name) + (prefixName ? ", " + prefixName : ""),
-      id: Math.floor(Math.random() * 999999999999),
-      phone: Number("8" + Math.floor(Math.random() * 9999999999)),
-      phone2: Number("8" + Math.floor(Math.random() * 9999999999)),
+      id,
+      phone,
+      ...(phone2 && { phone2 }),
       address,
       type,
       createdAt: serverTimestamp(),
@@ -37,9 +24,22 @@ async function handleAddCustomer(
       sales: auth.currentUser!.uid,
     };
 
+    // temporary, for testing
+    // const customer: Customer<object> = {
+    //   name: capitalizeWords(name) + (prefixName ? ", " + prefixName : ""),
+    //   id: Math.floor(Math.random() * 999999999999),
+    //   phone: Number("8" + Math.floor(Math.random() * 9999999999)),
+    //   phone2: Number("8" + Math.floor(Math.random() * 9999999999)),
+    //   address,
+    //   type,
+    //   createdAt: serverTimestamp(),
+    //   
+    //   sales: auth.currentUser!.uid,
+    // };
+
     await set(push(ref(database, "customers")), customer);
 
-    // navigate("/customers");
+    navigate("/customers");
   } catch (error: unknown) {
     if (error instanceof Error) {
       toast({
