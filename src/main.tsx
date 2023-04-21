51e821f3d86f2bb29fa689fbfbb980496bd5756c
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-04-21 16:26:43 +0700

    feat: add @vercel/analytics package and initialize it in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,16 @@
+import { Analytics } from "@vercel/analytics/react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-// import { StrictMode } from "react";
-
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-// const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root") as Element;
 
 ReactDOM.createRoot(rootElement).render(
-  // <StrictMode>
-  <BrowserRouter>
-    {/* <QueryClientProvider client={queryClient}> */}
-    <App />
-    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    {/* </QueryClientProvider> */}
-  </BrowserRouter>
-  // </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+      <Analytics />
+    </BrowserRouter>
+  </StrictMode>
 );
