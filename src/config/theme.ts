4dd041a5369fca0fb94f19845bbc389d8cda29b8
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-05-04 22:56:38 +0700

    refactor(theme.ts): reorder imports to follow alphabetical order and improve readability

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, defineStyleConfig, useMultiStyleConfig } from "@chakra-ui/react";
+import { defineStyleConfig, extendTheme } from "@chakra-ui/react";
 import background from "../assets/background.webp";
 
 import "@fontsource/inter/variable.css";
