0473c14cc3636195a455906e249589ed93270dae
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-05-06 23:52:41 +0700

    style(ContentWrapper.tsx): improve header style by adding background image and adjusting padding and margin feat(ContentWrapper.tsx): add dynamic icon to header based on current location refactor(ContentWrapper.tsx): simplify button rendering by spreading button props instead of manually setting colorScheme and variant

--- src/pages/dashboard/ContentWrapper.tsx
+++ src/pages/dashboard/ContentWrapper.tsx
@@ -1,27 +1,49 @@
-import { Heading, Stack, Box, Button, ButtonGroup } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Heading,
+  Stack,
+  useToken,
+} from "@chakra-ui/react";
+import { renderToString } from "react-dom/server";
+import { MdGroup, MdOutlineTextSnippet } from "react-icons/md";
+import { Link, useLocation } from "react-router-dom";
 
 type CWProps = {
   title: string;
   button?: {
     name: string;
     path: string;
-    colorScheme?: string;
-    variant?: string;
+    [key: string]: any;
   }[];
   children: React.ReactNode;
 };
 
 function ContentWrapper({ title, button, children }: CWProps) {
+  const location = useLocation().pathname;
+  const iconColor = useToken("colors", "secondary.50");
+  const iconComponent =
+    location === "/orders" ? (
+      <MdOutlineTextSnippet color={iconColor} />
+    ) : (
+      <MdGroup color={iconColor} />
+    );
+  const svgString = renderToString(iconComponent);
+  const svgUrl = `data:image/svg+xml,${encodeURIComponent(svgString)}`;
+
   return (
     <>
       <Box // header
-        bg="white"
         borderBottom="1px solid"
         borderBottomColor="gray.200"
         py="40px"
-        pl="50px"
-        pr="60px"
+        px={{ base: "30px", md: "50px" }}
+        bgColor="white"
+        bgImage={{base: "none", md: `${svgUrl}`}}
+        bgPosition="-20px 40%"
+        bgRepeat="no-repeat"
+        bgSize="250px"
       >
         <Stack
           justify="space-between"
@@ -36,12 +58,7 @@ function ContentWrapper({ title, button, children }: CWProps) {
             <ButtonGroup spacing={1}>
               {button.map((button) => (
                 <Link key={button.path} to={button.path}>
-                  <Button
-                    colorScheme={button.colorScheme}
-                    variant={button.variant}
-                  >
-                    {button.name}
-                  </Button>
+                  <Button {...button}>{button.name}</Button>
                 </Link>
               ))}
             </ButtonGroup>
