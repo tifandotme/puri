1fd0665303e71400efda795bff1f584f50638783
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-06-03 21:23:17 +0700

    refactor: remove @vercel/analytics package and replace it with a self-hosted analytics script to improve privacy and reduce external dependencies docs(index.html): add self-hosted analytics script to the head of the document to track website usage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import { inject } from "@vercel/analytics";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-if (import.meta.env.PROD) inject({ mode: "production" });
 
 const rootElement = document.getElementById("root") as Element;
 
