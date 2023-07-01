c926b006f29a9d997144a7e6b7b040a4d02a1205
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-01 22:57:47 +0700

    style: comment out env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  VITE_FIREBASE_API_KEY: string;
-  VITE_FIREBASE_APP_ID: string;
-  VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-}
+// interface ImportMetaEnv {
+//   VITE_FIREBASE_API_KEY: string;
+//   VITE_FIREBASE_APP_ID: string;
+//   VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+// }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
